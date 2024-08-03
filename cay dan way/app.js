318afa4ad6f3df410cafdf6ac309e0952d334e0a
Randi Febriadi <randifebriadi@gmail.com>
2024-08-03 08:32:50 +0700

    one line

--- cay dan way/app.js
+++ cay dan way/app.js
@@ -1,10 +1,12 @@
 function translatePigLatin(str) {
-  return str
-    .replace(/([aiueo])/i, " $1")
-    .split(" ")
-    .reverse()
-    .map((value) => (value.split("").some((char) => "aiueo".includes(char)) ? value : value ? value + "ay" : "way"))
-    .join("");
+  // return str
+  //   .replace(/([aiueo])/i, " $1")
+  //   .split(" ")
+  //   .reverse()
+  //   .map((value) => (value.split("").some((char) => "aiueo".includes(char)) ? value : value ? value + "ay" : "way"))
+  //   .join("");
+
+  return str.match(/^[aiueo]/i) ? str + "way" : str.replace(/([^aiueo]+)(\w+)/, "$2$1ay");
 }
 
 console.log(translatePigLatin("algorithm"));
